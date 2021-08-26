867a1deacacf392f113af519be6db1d717f13dc8
vanditvasa <vasavandit@gmail.com>
2021-08-26 20:55:47 +0530

    added splash screen

---
+++ app/src/main/java/cf/vandit/quizapp/SplashScreen.java
@@ -0,0 +1,19 @@
+package cf.vandit.quizapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(SplashScreen.this, MainActivity.class));
+        finish();
+    }
+}
